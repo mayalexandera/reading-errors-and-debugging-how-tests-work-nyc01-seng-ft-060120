fc716162676511f34c66af6985c6194b19f4a252
Maya Blevins <maya.blevins@gmail.com>
2020-05-04 19:06:16 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -9,3 +9,7 @@ second_number = 23
 sum = first_number + second_number
 
 difference = first_number - second_number
+
+product = first_number * second_number
+
+quotient = first_number / second_number
